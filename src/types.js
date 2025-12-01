5de3f71879433e5a2af6baf8135d3cbda29ac813
Abram137 <86285501+Abram137@users.noreply.github.com>
2025-12-01 22:18:38 +0300

    change js to ts

--- src/types.js
+++
@@ -1,8 +0,0 @@
-/**
- * @typedef {Object} AudioClip
- * @property {string} keyTrigger - The keyboard key to trigger this clip
- * @property {string} url - The URL of the audio file
- * @property {string} description - Display name for the clip
- */
-
-export const AudioClip = {};
